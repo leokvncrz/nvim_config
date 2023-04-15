459a8bf68ed53eb4b81af8a7a30b696fb57dae24
Zero Cruz <zero@keitri.com>
2023-04-15 09:08:44 +0800

    Initial Commit

---
+++ lua/zero/set.lua
@@ -0,0 +1,55 @@
+local opt = vim.opt
+
+-- line numbers
+opt.nu = true
+opt.relativenumber = true
+
+-- tabs & indentation
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+opt.smartindent = true
+
+-- line wrapping
+opt.wrap = false
+
+-- search settings
+opt.ignorecase = true
+opt.smartcase = true
+
+-- cursor linie
+opt.cursorline = true
+
+-- appearance
+opt.guicursor = ""
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+-- backspace
+opt.backspace = "indent,eol,start"
+
+-- clipboard
+opt.clipboard:append("unnamedplus")
+
+-- split windows
+opt.splitright = true
+opt.splitbelow = true
+
+opt.swapfile = false
+opt.backup = false
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+
+opt.hlsearch = false
+opt.incsearch = true
+
+
+-- opt.scrolloff = 8
+opt.isfname:append("@-@")
+
+opt.updatetime = 50
+
+-- opt.colorcolumn = "80"
